705a2c01bda6b36bd7d45a80598b75c45e017a79
barletjb <barletjb.perso@gmail.com>
2025-05-25 13:26:20 +0200

    Chapitre 1

---
+++ JavaCore/src/javacore/chapter01/variable/exercice/Biblioteque.java
@@ -0,0 +1,20 @@
+package javacore.chapter01.variable.exercice;
+
+public class Biblioteque {
+
+    public static void main (String[] args) {
+
+        long isbn = 1234567891234L;
+        short nbrePages = 5000;
+        byte rating = 5;
+        boolean available;
+        short nbrUsers = 2000;
+        float offence = 20.35F;
+        int password = 123456;
+
+
+
+
+
+    }
+}

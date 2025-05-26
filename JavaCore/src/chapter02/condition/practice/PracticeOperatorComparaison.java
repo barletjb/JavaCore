d38916c3eed17043c46278636c4d4813d7d5aa2a
barletjb <barletjb.perso@gmail.com>
2025-05-26 20:54:33 +0200

    Add files via upload

---
+++ JavaCore/src/chapter02/condition/practice/PracticeOperatorComparaison.java
@@ -0,0 +1,19 @@
+package javacore.chapter02.condition.practice;
+
+public class PracticeOperatorComparaison {
+
+    public static void main (String[] args) {
+
+        int a = 30;
+        int b = 40;
+
+        boolean isEqual = a == b;
+        System.out.println(isEqual);
+
+        boolean isNotEqual = a != b ;
+
+
+
+
+    }
+}

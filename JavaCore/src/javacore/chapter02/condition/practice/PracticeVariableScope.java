8b23334314ee9eb047820dff8b1bd4d594a465e6
barletjb <barletjb.perso@gmail.com>
2025-06-01 22:13:15 +0200

    Add files via upload

---
+++ JavaCore/src/javacore/chapter02/condition/practice/PracticeVariableScope.java
@@ -0,0 +1,20 @@
+package javacore.chapter02.condition.practice;
+
+public class PracticeVariableScope {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if (age < 18) {
+
+            System.out.println("Vous êtes mineurf");
+
+
+        }
+
+
+
+
+    }
+}

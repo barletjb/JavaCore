f1613deb8a8ddfd717505f6b75d0345673601460
barletjb <barletjb.perso@gmail.com>
2025-06-02 08:47:49 +0200

    Add files via upload

---
+++ JavaCore/src/javacore/chapter02/condition/exercice/MaxOfThree.java
@@ -0,0 +1,38 @@
+package javacore.chapter02.condition.exercice;
+
+public class MaxOfThree {
+
+    public static void main(String[] args) {
+
+        //Déclaration variables
+        int a = 10;
+        int b = 11;
+        int c = 13;
+
+        // 1ere étape : Vérification des égalités
+        if (a == b || a == c || b == c) {
+
+            if (a == b && a != c) {
+                System.err.println("Les valeurs de a et b sont égales.");
+            } else if (a == c && a != b) {
+                System.err.println("Les valeurs de a et c sont égales.");
+            } else if (b == c && b != a) {
+                System.err.println("Les valeurs de b et c sont égales.");
+            } else {
+                System.err.println("Toutes les valeurs sont égales, il n'y en a pas une de plus grande.");
+            }
+
+        }
+        // Si 0 égalité alors vérification des nbrs + grands
+        else if (a > b && a > c){
+            System.out.println("La valeur a : " + a + " , est la plus grande");
+
+        } else if (b > a && b > c) {
+            System.out.println("La valeur b : " + b + " , est la plus grande");
+        }
+        else {
+            System.out.println("La valeur c : " + c + " , est la plus grande");
+        }
+
+    }
+}

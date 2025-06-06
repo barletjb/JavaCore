920b37b98f2ad57a31a1f1defbf33d7ca013ed34
barletjb <barletjb.perso@gmail.com>
2025-06-06 12:51:31 +0200

    Add files via upload
    Ternaire

---
+++ JavaCore/src/javacore/chapter02/condition/exercice/ConditionToTernary.java
@@ -0,0 +1,27 @@
+package javacore.chapter02.condition.exercice;
+
+public class ConditionToTernary {
+
+    public static void main(String[] args) {
+
+        /**
+         * Le code doit s'adapter aux différentes valeurs que vous mettrez à la variable age.
+         */
+        int age = 12;
+
+        int pourcentageReduction = 0;
+
+        if (age < 18) {
+            pourcentageReduction = 50;
+        } else {
+            pourcentageReduction = 10;
+        }
+
+        System.out.println("La réduction appliquée est de " + pourcentageReduction + "% car l'utilisateur est agé de " + age + " ans");
+
+        // Ternaire
+        pourcentageReduction = age < 18 ? 50 : 10;
+        System.out.println("La réduction appliquée est de " + pourcentageReduction + "% car l'utilisateur est agé de " + age + " ans");
+    }
+}
+

36e309c2651178b0e68bd8e49c5cd14632230c2d
barletjb <barletjb.perso@gmail.com>
2025-07-07 08:55:57 +0200

    Add files via upload

---
+++ JavaCore/src/javacore/chapter05/exercice/Pen.java
@@ -0,0 +1,36 @@
+package javacore.chapter05.exercice;
+
+public class Pen {
+
+    String name;
+    String color;
+    String company;
+
+    double price;
+    double stock;
+    double weight;
+
+    public void displayInformation() {
+
+        System.out.println("Nom: " + name + " | Couleur : " + color +
+                " | Prix : " + price + " | Quantité : " + stock
+                + " | Fabricant : " + company + " | Poids : " + weight + "g");
+
+    }
+
+    public static void main(String[] args) {
+
+        Pen pen1 = new Pen();
+        pen1.name = "stylo";
+        pen1.color = "Bleu";
+        pen1.company = "BIC";
+        pen1.price = 2.50;
+        pen1.stock = 10;
+        pen1.weight = 15;
+
+        pen1.displayInformation();
+
+
+    }
+
+}
